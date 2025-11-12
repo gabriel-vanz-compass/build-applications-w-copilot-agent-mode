a01d92ed104da390a15cbfb049f2c5fa81980789
Gabriel Vanz <gabriel.vanz@compasso.com.br>
2025-11-12 17:48:46 +0000

    Add Etapa 3

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,39 @@
+from djongo import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='octofit_users',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='octofit_users_permissions',
+        blank=True
+    )
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    user_email = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
